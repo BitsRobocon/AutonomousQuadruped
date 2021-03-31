5c0abfa31ae99ca0812883ac6d1b1db2d3686879
Ekansh Gupta <ekanshgupta92@gmail.com>
2021-03-31 14:58:40 +0530

    Fixed urdf path

--- src/quadruped/quadruped/pybullet_data/urdf.py
+++ src/quadruped/quadruped/pybullet_data/urdf.py
@@ -6,7 +6,7 @@ phisicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) #used by loadURDF
 
 planeId = p.loadURDF("plane.urdf")
-quadID = p.loadURDF("src/quadruped/quadruped/pybullet_data/vrka.urdf")
+quadID = p.loadURDF("./vrka.urdf")
 GRAVITY = -9.8
 
 p.setGravity(0, 0, -10)
